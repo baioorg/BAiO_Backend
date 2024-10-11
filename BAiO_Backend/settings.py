21114b77a8ab2b820b41366549a6521b6e419e9e
Kai.Waloen <kawal5842@uib.no>
2024-10-11 19:07:27 +0200

    building userAuth
    Co-authored-by: Trym Kvitnes <TBK-Iron@users.noreply.github.com>
    Co-authored-by: stianbekk <stianbekk@users.noreply.github.com>

--- BAiO_Backend/settings.py
+++ BAiO_Backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "BAiO_App",
+    "userAuth"
 ]
 
 MIDDLEWARE = [
--- userAuth/models.py
+++ userAuth/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class User(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
@@ -38,36 +37,3 @@ class User(models.Model):
     
     def __str__(self):
         return self.username
-    
-class LLM(models.Model):
-    title = models.CharField(max_length=150)
-
-    def __str__(self):
-        return self.title
-    
-class ApiKey(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=150)
-    key = models.CharField(max_length=128)
-    LargeLanguageModel = models.ForeignKey(LLM, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-    
-    def get_key_info(self):
-        return {
-            'key': self.key,
-            'date_created': self.date_created,
-            'is_active': self.is_active,
-        }
-    
-# class Message(models.Model):
-#     text = models.TextField()
-#     date_sent = models.DateTimeField(auto_now_add=True)
-    
-#     def __str__(self):
-#         return self.title
-
-# class UserMessage(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     message = Message
-
