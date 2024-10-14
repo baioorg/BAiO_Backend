d87077a3f40a19a84653bb0fe0b1ff29de40ac09
Trym Kvitnes <trymbkvitnes@gmail.com>
2024-10-14 18:02:13 +0200

    Merge remote-tracking branch 'origin/together' into trym-dev

--- userAuth/serializers.py
+++ userAuth/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from .models import User
-    
+
+# Serializer for user authentication
 class UserAuthSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
+        # Authenticate user with provided username and password
         username = data.get('username')
         password = data.get('password')
 
@@ -16,9 +18,10 @@ class UserAuthSerializer(serializers.Serializer):
             raise serializers.ValidationError(f"Invalid credentials for {username} and {password}")
         
         if not user.is_active:
-            raise serializers.ValidationError("User account is diabled")
+            raise serializers.ValidationError("User account is disabled")
         return user
-    
+
+# Serializer for user registration
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -38,6 +41,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         }
 
     def create(self, val_data):
+        # Create a new user instance with validated data
         user = User.objects.create_user(
             username=val_data['username'],
             email=val_data['email'],
@@ -50,7 +54,8 @@ class RegisterSerializer(serializers.ModelSerializer):
             field_of_study=val_data['field_of_study']
         )
         return user
-    
+
+# Serializer for user information
 class InfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = User 
